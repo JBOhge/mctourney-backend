68d6d1e50220db158a4ae7dfc05e98247297ed5d
JBOhge <ohgejb@gmail.com>
2022-08-30 11:12:39 -0500

    removed socketio

--- server.js
+++ server.js
@@ -4,19 +4,13 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
 
 mongoose.connect(process.env.DATABASE, { useNewUrlParser: true }).then(() => {
   console.log('db connection successful');
 });
 
-io.on('connection', (socket) => {
-  console.log('user connected');
-});
-
 const port = process.env.PORT;
 
-http.listen(port, () => {
+app.listen(port, () => {
   console.log('App Started...');
 });
